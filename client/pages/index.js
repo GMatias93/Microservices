cef3f0d91c821675d3ffc2d230820965a56d6ea7
gmatias93 <gabriel.2m.deleon@gmail.com>
2021-11-08 17:11:49 -0500

    enable SSR fetch current-user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+	console.log(currentUser);
+
 	return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async context => {
+	const { data } = await buildClient(context).get('/api/users/current-user');
+
+	return data;
+};
+
+export default LandingPage;
