3a62240030b44e2c1079fa0f5363b811ad1a7480
gmatias93 <gabriel.2m.deleon@gmail.com>
2021-11-14 17:41:47 -0500

    putting stuff back in auth

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,11 +3,6 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@gmticketing/common';
 
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/sign-in';
-import { signOutRouter } from './routes/sign-out';
-import { signupRouter } from './routes/signup';
-
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -17,11 +12,6 @@ app.use(
 		secure: process.env.NODE_ENV !== 'test',
 	})
 );
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signupRouter);
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError();
